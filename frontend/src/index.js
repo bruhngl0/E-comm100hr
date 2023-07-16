1097e54a4b39cf2c7d141ddc5a39347d0f8ab3fb
bruh.ngl0 <adityashrm500@gmail.com>
2023-07-16 14:10:13 +0530

    redux section complete

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -4,6 +4,8 @@ import  {createBrowserRouter, createRoutesFromElements, Route, RouterProvider } 
 
 import './assets/styles/bootstrap.custom.css'
 import './assets/styles/index.css';
+import store from '../src/store';
+import { Provider } from 'react-redux'
 //import 'bootstrap/dist/css/bootstrap.min.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
@@ -24,8 +26,9 @@ const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
    
+   <Provider store = {store}>
     <RouterProvider router= {router} />
-   
+    </Provider>
   </React.StrictMode>
 );
 
--- frontend/src/screens/HomeScreen.jsx
+++ frontend/src/screens/HomeScreen.jsx
@@ -1,27 +1,21 @@
 import React from 'react'
-import {useEffect, useState} from 'react'
 import {Row, Col} from 'react-bootstrap'
 import Product from '../components/Product'
+import { useGetProductsQuery } from '../slices/productsApiSlice'
 
-import axios from 'axios'
+import Loader from '../components/Loader'
+import Message from '../components/Message'
 
 
 
 const HomeScreen = () => {
 
-
-  const [products, setProducts] = useState([]);
-
-  useEffect(()=> {
-    const fetchProducts = async () => {
-     const { data } = await axios.get('/api/products')
-     setProducts (data) 
-    };
-
-    fetchProducts()
-  },[])
+const {data: products , isLoading, error } = useGetProductsQuery();
+ 
   return (
     <>
+
+    {isLoading ? (<Loader />) : error ? (<Message variant = 'danger'>{error?.data?.message || error.error}</Message>) : (<>
       <h1>Latest Products</h1>
       <Row>
         {products.map((product)=> (
@@ -30,6 +24,8 @@ const HomeScreen = () => {
             </Col>
         ))}
       </Row>
+      </>)}
+      
     </>
   )
 }
