573690ef07fb531c011f504a7e6d0f1a3f4e6e29
bruh.ngl0 <adityashrm500@gmail.com>
2023-07-25 22:58:28 +0530

    login

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -3,6 +3,8 @@ import {Container} from 'react-bootstrap'
 
 import Header from './components/Header'
 import { Outlet } from 'react-router-dom'
+import { ToastContainer } from 'react-toastify'
+import 'react-toastify/dist/ReactToastify.css'
 import Footer from './components/Footer'
 
 
@@ -16,6 +18,7 @@ const App = () => {
       </Container>
     </main>
     <Footer />
+    <ToastContainer />
 
   </>
   )
